ad642c1c057658e5307c9ff881cb1ae675dcb382
johnhamm <johnjhamm@gmail.com>
2014-09-21 23:51:02 -0400

    Concurrency tester cmd line to swith to numeric

--- examples/Concurrencytester.js
+++ examples/Concurrencytester.js
@@ -1,9 +1,15 @@
-﻿var generator = require('./lib/snowmaker'),
+﻿// Run multiple instances of this to test concurrency
+// e.g. to generate 1000 ints with 125ms delay inbetween:
+// node Concurrencytester.js 1000 125 true
+
+var generator = require('../lib/snowmaker'),
     async = require('async'),
     fs = require('fs');
 
 var maxCount = Number(process.argv[2]) || 1000,
     delay = Number(process.argv[3]) || 250;
+
+    generator.alphanumeric = !(process.argv[4] && process.argv[4] === "true");
 
 generator.connect("UseDevelopmentStorage=true;", function (err) {
     var count = 0;
